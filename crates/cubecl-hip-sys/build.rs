cb804b82f3349134d9a14798c4b4cd88f9918df4
syl20bnr <sylvain.benner@tracel.ai>
2024-11-20 22:50:39 -0500

    Add default path for ROCm installation

--- crates/cubecl-hip-sys/build.rs
+++ crates/cubecl-hip-sys/build.rs
@@ -7,12 +7,13 @@ fn main() {
     println!("cargo:rerun-if-env-changed=ROCM_PATH");
     println!("cargo:rerun-if-env-changed=HIP_PATH");
 
-    let env_vars: Vec<_> = ["CUBECL_ROCM_PATH", "ROCM_PATH", "HIP_PATH"]
+    let mut paths: Vec<_> = ["CUBECL_ROCM_PATH", "ROCM_PATH", "HIP_PATH"]
         .into_iter()
         .filter_map(|var| env::var(var).ok())
         .collect();
+    paths.push("/opt/rocm".to_string());
 
-    let rocm_path = env_vars.iter().find(|path| {
+    let rocm_path = paths.iter().find(|path| {
         let hip_path = Path::new(path).join("include/hip");
         hip_path.exists()
     });
@@ -21,7 +22,7 @@ fn main() {
         println!("cargo:rustc-link-lib=dylib=hiprtc");
         println!("cargo:rustc-link-lib=dylib=amdhip64");
         println!("cargo:rustc-link-search=native={}/lib", valid_rocm_path);
-    } else if !env_vars.is_empty() {
+    } else if !paths.is_empty() {
         panic!("HIP headers not found in any of the defined CUBECL_ROCM_PATH, ROCM_PATH or HIP_PATH directories.");
     }
 }
